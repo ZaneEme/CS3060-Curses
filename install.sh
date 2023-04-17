c1409b93fab7862d3fb44ed378373ab3f8aac710
Zane Emerick <Zane.Emerick@kratosdefense.com>
2023-04-17 08:19:56 -0600

    added checks for installed dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 rm -rf build
 
+if ! cmake --version > /dev/null; then
+    echo "Cmake not installed."
+    echo "To install it please run:"
+    echo "sudo apt install cmake"
+    return 1;
+fi
+
 mkdir build && cd build
 
 cmake ..
+
+if ! make -v > /dev/null; then
+    echo "make not installed."
+    echo "To install it please run:"
+    echo "sudo apt install make"
+    return 1;
+fi
 
 make
--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 cd build
 
+if ! make -v > /dev/null; then
+    echo "make not installed."
+    echo "To install it please run:"
+    echo "sudo apt install make"
+    return 1;
+fi
+
 make
 
 cd ..
